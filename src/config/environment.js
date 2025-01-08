1d0d0503683d8e2dbe6fc517eb57b9a36ec6f906
minhnguyencong1711 <minhnguyencong333@gmail.com>
2025-01-08 12:16:12 +0700

    update environment

--- src/config/environment.js
+++ src/config/environment.js
@@ -8,6 +8,9 @@ export const env = {
     APP_HOST: process.env.APP_HOST,
     APP_PORT: process.env.APP_PORT,
 
+    JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+    JWT_REFRESH_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+
     BUILD_MODE: process.env.BUILD_MODE,
     
     AUTHOR: process.env.AUTHOR
