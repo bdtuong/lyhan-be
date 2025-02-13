3eafeff660365572b5ce671357cc2d86198df7de
waanney <work.buiminhquan@gmail.com>
2025-02-14 00:16:42 +0700

    fixed: cors

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 export const WHITELIST_DOMAINS = [
     'http://localhost:5173',
-    'https://scout-h8z4.onrender.com/login'
+    'https://scout-h8z4.onrender.com'
     // những domain có thể nhận database
 ]
