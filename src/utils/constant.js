2db63e5a8ed9ebb833895f413f142815c1812301
waanney <work.buiminhquan@gmail.com>
2025-02-14 04:33:44 +0700

    fix

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,6 @@
 import {env} from '../config/environment.js'
 export const WHITELIST_DOMAINS = [
     env.FRONTEND_URL,
+    'https://scout-backend-tvhe.onrender.com',
     // những domain có thể nhận database
 ]
