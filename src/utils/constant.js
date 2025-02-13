75e4738aa5efd415694fe52d6b2de2e6ea45c1ca
waanney <work.buiminhquan@gmail.com>
2025-02-14 06:05:58 +0700

    fix

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,8 @@
 import {env} from '../config/environment.js'
 export const WHITELIST_DOMAINS = [
     env.FRONTEND_URL,
-    'http://localhost:5173',
+    
+
 
     // những domain có thể nhận database
 ]
