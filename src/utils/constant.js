2f7a9947e1da4c9e4583951a37a9c0409d48f834
waanney <work.buiminhquan@gmail.com>
2025-02-14 00:20:10 +0700

    fixed: agian

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,4 @@
 export const WHITELIST_DOMAINS = [
-    'http://localhost:5173',
-    'https://scout-h8z4.onrender.com'
+    'https://scout-h8z4.onrender.com',
     // những domain có thể nhận database
 ]
