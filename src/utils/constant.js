5f0c95116c92f71c3f8aedbb5b714983f274def8
waanney <work.buiminhquan@gmail.com>
2025-02-28 02:06:42 +0700

    update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,6 @@
 import { env } from '~/config/environment.js';
 export const WHITELIST_DOMAINS = [
   env.FRONTEND_URL,
-  env.CLOUDINARY_URL,
-  'https://res.cloudinary.com/dzzpvjxsh/image/upload',
   // những domain có thể nhận database
 ];
   
