0a179721768a3403d03ac5d9c35b2e6a719320a8
waanney <work.buiminhquan@gmail.com>
2025-03-03 01:34:58 +0700

    update

--- src/routes/v1/notificationRoute.js
+++ src/routes/v1/notificationRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { notificationController } from '~/controllers/UsersCollection/NotificationController.js';
+import {notificationController} from '../../controllers/UsersCollection/notificationController'
 
 const Router6 = express.Router();
 
